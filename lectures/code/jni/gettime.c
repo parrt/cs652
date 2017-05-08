7a5bad76099c2a41f3885cc2010fd990079bc62b
parrt <parrt@cs.usfca.edu>
2017-05-08 14:55:09 -0700

    update jvm slides

--- lectures/code/jni/gettime.c
+++ lectures/code/jni/gettime.c
@@ -7,15 +7,15 @@
 
 JNIEXPORT jstring JNICALL Java_GetTime_getTime(JNIEnv *env, jobject obj)
 {
-    static char timeStr[TIME_STR_LEN];
-    static jstring jtime;
+    static char timeStr[TIME_STR_LEN]; // make non-local space
+    static jstring jtime;              // alloc local struct space
     time_t current;
     struct tm *t;
 
-    current = time((time_t *) NULL);
+    current = time((time_t *) NULL);   // call OS
     t = localtime(&current);
     sprintf(timeStr, "The current time is: %02d:%02d:%02d.\n",
-    t->tm_hour, t->tm_min, t->tm_sec);
-    jtime = (*env)->NewStringUTF(env, timeStr);
-    return jtime;
+            t->tm_hour, t->tm_min, t->tm_sec);
+    jtime = (*env)->NewStringUTF(env, timeStr); // C->Java string
+    return jtime;                      // return copy of local struct
 }
